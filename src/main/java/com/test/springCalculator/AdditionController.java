30e96a64cc4ab44deebc27f10441005d10a211f1
naveen <navin5556@gmail.com>
2021-07-20 19:46:12 +0530

    first java program

---
+++ src/main/java/com/test/springCalculator/AdditionController.java
@@ -0,0 +1,56 @@
+package com.test.springCalculator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdditionController {
+	@RequestMapping("add")
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+		int a = Integer.parseInt(request.getParameter("t1"));
+		int b = Integer.parseInt(request.getParameter("t2"));
+		int c = a + b;
+		ModelAndView model=new ModelAndView();
+		model.setViewName("resultPage");
+		model.addObject("result", c);
+		model.addObject("display", "add");
+		return model;
+	}
+	@RequestMapping("subtract")
+	public ModelAndView subtract(HttpServletRequest request, HttpServletResponse response) {
+		int a = Integer.parseInt(request.getParameter("t1"));
+		int b = Integer.parseInt(request.getParameter("t2"));
+		int c = a - b;
+		ModelAndView model=new ModelAndView();
+		model.setViewName("resultPage");
+		model.addObject("result", c);
+		model.addObject("display", "subtract");
+		return model;
+	}
+	@RequestMapping("multiply")
+	public ModelAndView multiply(HttpServletRequest request, HttpServletResponse response) {
+		int a = Integer.parseInt(request.getParameter("t1"));
+		int b = Integer.parseInt(request.getParameter("t2"));
+		int c = a * b;
+		ModelAndView model=new ModelAndView();
+		model.setViewName("resultPage");
+		model.addObject("result", c);
+		model.addObject("display", "multiply");
+		return model;
+	}
+	@RequestMapping("divide")
+	public ModelAndView divide(HttpServletRequest request, HttpServletResponse response) {
+		int a = Integer.parseInt(request.getParameter("t1"));
+		int b = Integer.parseInt(request.getParameter("t2"));
+		int c = a / b;
+		ModelAndView model=new ModelAndView();
+		model.setViewName("resultPage");
+		model.addObject("result", c);
+		model.addObject("display", "divide");
+		return model;
+	}
+}
---
+++ src/main/webapp/resultPage.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1" isELIgnored="False"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Calculator</title>
+<style>
+div {
+	border: 3px solid blue;
+	padding-right: 250px;
+	background-color: yellow;
+}
+</style>
+</head>
+<body>
+	<div>${display} : ${result}</div>
+
+</body>
+</html>
